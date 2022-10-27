ed46022fdebca324d9352e5eb648e56e252afeb2
saboorqais <saboor.qaiser456@gmail.com>
2022-10-27 05:07:40 +0500

    Added modules of css

--- components/MainContainer/MainContainer.js
+++ components/MainContainer/MainContainer.js
@@ -1,23 +1,23 @@
 import React from "react";
 import FieldContainer from "../FieldContainer/FieldContainer";
-
+import styles from '../MainContainer/MainContainer.module.css'
 import PaginationPage from "../Pagination/PaginationPage";
 
 function MainContainer() {
   return (
-    <div className="MainContainer">
-      <div className="HeadingContainer">
-        <div className="Heading">Demo Processing</div>
-        <div className="TabsHeading">
-          <div className="IndividualTab TypographyTabs">
+    <div className={styles.MainContainer}>
+      <div className={styles.HeadingContainer}>
+        <div className={styles.Heading}>Demo Processing</div>
+        <div className={styles.TabsHeading}>
+          <div className={`${styles.IndividualTab} TypographyTabs`}>
             Demo Reminders/Calling
           </div>
-          <div className="IndividualTab ">Client Formalities</div>
-          <div className="IndividualTab ">T Formalities</div>
-          <div className="IndividualTab ">Day-1 Processing</div>
-          <div className="IndividualTab ">Day-2 Processing</div>
-          <div className="IndividualTab ">Extended Demo</div>
-          <div className="IndividualTab ">Client Reviewing</div>
+          <div className={`${styles.IndividualTab} TypographyTabs`}>Client Formalities</div>
+          <div className={`${styles.IndividualTab} TypographyTabs`}>T Formalities</div>
+          <div className={`${styles.IndividualTab} TypographyTabs`}>Day-1 Processing</div>
+          <div className={`${styles.IndividualTab} TypographyTabs`}>Day-2 Processing</div>
+          <div className={`${styles.IndividualTab} TypographyTabs`}>Extended Demo</div>
+          <div className={`${styles.IndividualTab} TypographyTabs`}>Client Reviewing</div>
         </div>
       </div>
       <FieldContainer/>
